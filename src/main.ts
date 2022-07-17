3eac8e6947156dbb2a5160fe02884ceda3f4950e
Gentrit Uka <gentrit-uka@live.com>
2022-07-17 11:52:18 +0200

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+document.addEventListener('DOMContentLoaded', () => {
+    let bird = document.querySelector('.bird')
+    let gameDisplay = document.querySelector('.game-container')
+    let ground = document.querySelector('.ground')
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+    let birdLeft = 220
+    let birdBottom = 100
+})
