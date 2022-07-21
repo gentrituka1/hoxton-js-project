fa15c7eed3f5c3714681e1b6beebab2fde64bc24
Gentrit Uka <gentrit-uka@live.com>
2022-07-21 17:08:12 +0200

    updated

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ let bird = document.querySelector(".bird");
 let gameDisplay = document.querySelector(".game-container");
 let ground = document.querySelector(".ground");
 let sky = document.querySelector(".sky");
+let overSky = document.querySelector(".over-sky");
 
 let bestScore = Number(localStorage.bestScore) || 0;
 let birdLeft = 220;
@@ -15,7 +16,8 @@ let gap = 500;
 let count = 0;
 let obstacleBottom = 150;
 let isGameOver = false;
-let obstacleTimeoutID = null
+let obstacleTimeoutID = null;
+let leaderboardArray = [];
 
 function startGame() {
     birdBottom -= gravity;
@@ -82,7 +84,7 @@ function generateObstacle() {
             clearInterval(timerId);
             gameOver();
         }
-        if (obstacleLeft < 220 && obstacleLeft > 217) {
+        if (obstacleLeft < 220 && obstacleLeft > 216) {
             countPoints();
         }
     }
@@ -108,20 +110,112 @@ function gameOver() {
         bestScore = count;
         localStorage.bestScore = count;
     }
-
+    
 }
 
-function renderUsername(){
-    let username = document.createElement("input");
-    username.className = "username";
-    username.placeholder = "Enter your username";
-    username.addEventListener('submit', function(event){
-        event.preventDefault();
-        let username = document.querySelector(".username");;
-        localStorage.username = username?.ariaValueText;
-        username.style.display = "none";
+function postLeaderboardItem(username: string, score: number) {
+    fetch('http://localhost:4000/leaderboard', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            score: score
+        })
+    }).then(response => { return response.json(); })
+}
+
+function getLeaderboardItems(){
+    fetch('http://localhost:4000/leaderboard', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => { return response.json(); })
+    .then(data => {
+        leaderboardArray = data;
     })
-    gameContainer.append(username);
+}
+
+function renderLeaderboard() {
+
+    let leaderboard = document.createElement("section");
+    leaderboard.className = "leaderboard";
+    
+    let leaderboardName = document.createElement("h2");
+    leaderboardName.className = "leaderboard-name";
+    leaderboardName.textContent = "Leaderboard";
+
+    let leaderboardHeader = document.createElement("header");
+    leaderboardHeader.className = "leaderboard-header";
+
+    let rank = document.createElement("span");
+    rank.textContent = "ID";
+    let playerName = document.createElement("span");
+    playerName.textContent = "Player Name";
+    let score = document.createElement("span");
+    score.textContent = "Score";
+
+    leaderboardHeader.append(rank, playerName, score);
+
+    let leaderboardData = document.createElement("main");
+    leaderboardData.className = "leaderboard-data";
+
+    for(let element of leaderboardArray){
+
+      if (element.username !== localStorage.name){
+
+      let playerData = document.createElement("ul");
+      playerData.className = "player-data";
+
+      let playerDataItem = document.createElement("li");
+      playerDataItem.textContent = element.id;
+
+      let playerDataItem2 = document.createElement("li");
+      playerDataItem2.textContent = element.username;
+
+      let playerDataItem3 = document.createElement("li");
+      playerDataItem3.textContent = element.score;
+
+      playerData.append(playerDataItem, playerDataItem2, playerDataItem3);
+      leaderboardData.append(playerData);
+      }
+  }
+
+    let restartLeaderboard = document.createElement("button");
+    restartLeaderboard.className = "restart-button";
+    restartLeaderboard.textContent = "Restart Leaderboard";
+    restartLeaderboard.addEventListener("click", function(){
+      // loop the json array and delete all items
+
+      for(let element of leaderboardArray){
+        fetch('http://localhost:4000/leaderboard/' + element.id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => { return response.json(); })
+      }
+      getLeaderboardItems();
+
+  })
+
+    let restartButton = document.createElement("button");
+    restartButton.className = "restart-button";
+    restartButton.textContent = "Restart Game";
+    restartButton.addEventListener("click", () => {
+        location.reload();
+    })
+
+    let divEl = document.createElement("div");
+    divEl.className = "leaderboardWhole";
+
+    
+    
+    divEl.append(leaderboardHeader, leaderboardData)
+    leaderboard.append(leaderboardName, divEl, restartLeaderboard, restartButton);
+    document.body.append(leaderboard);
 }
 
 function lostRender() {
@@ -167,12 +261,42 @@ function lostRender() {
         let buttonEl2 = document.createElement("button");
         buttonEl2.className = "button";
         buttonEl2.textContent = "LEADERBOARD";
+        buttonEl2.addEventListener("click", () => {
+          document.body.innerHTML = ''
+            renderLeaderboard();
+        }, {once : true});
+
+        let form = document.createElement("form");
+        form.className = "username-form";
+        form.addEventListener('submit', function(event){
+          event.preventDefault();
+          
+          localStorage.username = username.value;
+          
+          postLeaderboardItem(username.value, bestScore);
+
+          username.style.display = "none";
+
+          let h2El = document.createElement("h2");
+          h2El.className = "leaderboard-check";
+          h2El.textContent = `Done Check Leaderboard`;
+
+          divEl2.append(h2El);
+
+          getLeaderboardItems();
+        })
+
+        let username = document.createElement("input");
+        username.className = "username";
+        username.placeholder = "Enter your username";
+    
+        form.append(username);
 
         //if we need leaderboard table button
 
         divEl3.append(buttonEl1, buttonEl2);
         divEl2.append(divEl3);
-        sectionEl.append(divEl1, divEl2);
+        sectionEl.append(divEl1, divEl2, form);
 
         sky?.append(sectionEl);
     }
@@ -185,5 +309,7 @@ function countPoints() {
     scorePoints.className = "score-points";
     scorePoints.textContent = `Score: ${++count}`;
 
-    score.append(scorePoints);
+    score?.append(scorePoints);
 }
+
+getLeaderboardItems();
